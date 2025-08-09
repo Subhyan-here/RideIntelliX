67204b825844adec3085652698cded4544d21bea
tanisha130 <taniroy2020@gmail.com>
2025-08-09 13:38:27 +0530

    updated components and pages folder under client

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import VehicleDetails from './pages/VehicleDetails'
+import Vehicles from './pages/Vehicles'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
+
+const [showLogin, setShowLogin] = useState(false)
+const isOwnerPath = useLocation().pathname.startsWith('/owner') 
+
   return (
-    <div>App</div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/vehicle-details/:id'element={<VehicleDetails/>}/>
+        <Route path='/vehicles'element={<Vehicles/>}/>
+        <Route path='/my-bookings'element={<MyBookings/>}/>
+
+        
+      </Routes>
+    </>
   )
 }
 
