b582213236d4a03c4af21880aea7224cc1a54d17
Tamajeet466 <ghoshtamajeet914@gmail.com>
2025-08-17 03:54:34 +0530

    Mybookings.jsx and Vehicles.jsx

--- client/src/pages/Vehicles.jsx
+++ client/src/pages/Vehicles.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
+import { assets, dummyCarData } from '../assets/assets'
+import VehicleCard from '../components/VehicleCard'
 
 const Vehicles = () => {
 
@@ -11,10 +13,11 @@ const Vehicles = () => {
       <div className='flex flex-col items-center py-20 bg-light max-md:px-4'>
         <Title title='Available vehicles' subTitle='Browse our selection of premium vehicles available for your next adventure'/>
 
-      <div className='flex items-center bg-whitw px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
-            <img src={assets.search_icon} alt="" className='w-4.5 h-4.5 mr-2'/>
+      <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 
+      rounded-full shadow'>
+        <img src={assets.search_icon} alt="" className='w-4.5 h-4.5 mr-2'/>
 
-            <input onClick={(e)=> setInput(e.target.value)} value={input} type="text" 
+        <input onClick={(e)=> setInput(e.target.value)} value={input} type="text" 
             placeholder='Search by make, model, or features' className='w-full h-full 
             outline-none text-gray-500' />
             
@@ -22,7 +25,19 @@ const Vehicles = () => {
       </div>
     </div>
 
-      <div></div>
+      <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-10'>
+      <p className='text-gray-500 xl:px-20 max-w-7x1 mx-auto'>Showing 
+        {dummyCarData.length} Vehicles</p>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7x1 mx-auto'>
+        {dummyCarData.map((vehicle, index)=> (
+          <div key={index}>
+            <VehicleCard vehicle={vehicle} />
+          </div>
+        ))}
+
+      </div>
+      </div>
 
     </div>
   )
