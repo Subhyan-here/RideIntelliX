a8d46e2ac551c554bff44174124c2283f6919c88
Tamajeet466 <ghoshtamajeet914@gmail.com>
2025-08-22 04:24:41 +0530

    updated

--- FRONTEND/client/src/assets/assets.js
+++ FRONTEND/client/src/assets/assets.js
@@ -43,6 +43,12 @@ import car_image3 from "./car_image3.png"
 import car_image4 from "./car_image4.png"
 import car_image5 from "./car_image5.png"
 import car_image6 from "./car_image6.png"
+import car_image7 from "./car_image7.png"
+import car_image8 from "./car_image8.png"
+import car_image9 from "./car_image9.png"
+import car_image10 from "./car_image10.png"
+import car_image11 from "./car_image11.png"
+import car_image12 from "./car_image12.png"
 
 export const cityList = ['Delhi', 'Mumbai', 'Chennai', 'Kolkata', 'Hydrabad']
 
@@ -91,7 +97,13 @@ export const assets = {
     car_image3,
     car_image4,
     car_image5,
-    car_image6
+    car_image6,
+    car_image7,
+    car_image8,
+    car_image9,
+    car_image10,
+    car_image11,
+    car_image12,
 }
 
 export const menuLinks = [
@@ -187,7 +199,7 @@ export const dummyCarData = [
     {
         "_id": "67ff5bc069c03d4e45f30b02",
         "owner": "67fe3467ed8a8fe17d0ba6e2",
-        "brand": "Randi",
+        "brand": "OLA",
         "model": "Roadster",
         "image": car_image6,
         "year": 2025,
@@ -217,7 +229,109 @@ export const dummyCarData = [
         "description": "Royal Enfield Classic 350 DarkThe Royal Enfield Classic 350 is a retro-styled motorcycle known for its classic design and comfortable riding experience. It features a 349cc engine, a twin downtube spine frame, and a comfortable, upright riding posture. ",
         "isAvaliable": true,
         "createdAt": "2025-04-16T07:26:56.215Z",
-    }
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "TVS",
+        "model": "Apache RTR 180",
+        "image": car_image7,
+        "year": 2024,
+        "category": "Bike",
+        "user_name": "Gurpreet Singh",
+        "fuel_type": "Petrol",
+        "transmission": "Semi-Automatic",
+        "pricePerDay": 1270,
+        "location": "Delhi",
+        "description": "The TVS Apache RTR 180 is a sporty motorcycle designed for performance and agility, featuring a 177.4 cc single-cylinder, 4-stroke, air-cooled engine that delivers a maximum power output of 17.63 PS at 9,250 rpm and a peak torque of 14.73 Nm at 7,250 rpm. ",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "TVS",
+        "model": "Jupiter",
+        "image": car_image8,
+        "year": 2023,
+        "category": "Scooter",
+        "user_name": "Raghu Ram",
+        "fuel_type": "Petrol",
+        "transmission": "Semi-Automatic",
+        "pricePerDay": 800,
+        "location": "Chennai",
+        "description": "The TVS Jupiter is a popular scooter model launched by TVS Motor Company in India in September 2013, designed as a rival to the Honda Activa. It has become one of the highest-selling scooters in India, second only to the Honda Activa, and is known for its comfort, fuel efficiency, and practicality. ",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "TVS",
+        "model": "iQube",
+        "image": car_image9,
+        "year": 2025,
+        "category": "Scooter",
+        "user_name": "Ashish Sharma",
+        "fuel_type": "Hybrid",
+        "transmission": "Semi-Automatic",
+        "pricePerDay": 850,
+        "location": "Hydrabad",
+        "description": "TVS Motor Company has recently launched the iQube Hybrid 2025, a new hybrid scooter combining a 110 cc petrol engine with a 4.4 kW electric motor, offering a 200 km range.",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "Honda",
+        "model": "SP160",
+        "image": car_image10,
+        "year": 2024,
+        "category": "Bike",
+        "user_name": "Naman Marthur",
+        "fuel_type": "Petrol",
+        "transmission": "Semi-Automatic",
+        "pricePerDay": 1300,
+        "location": "Kolkata",
+        "description": " The Honda SP 160 is a premium commuter motorcycle designed to offer aspirational aesthetics, commendable performance, and a range of desirable features, positioned between the Honda SP 125 and the Unicorn in Honda's lineup. It is powered by a 162.71cc BS6 Phase 2 engine, producing 13.27 bhp and 14.58 Nm of torque, mated to a five-speed manual gearbox.",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "Ather",
+        "model": "450",
+        "image": car_image11,
+        "year": 2024,
+        "category": "Scooter",
+        "user_name": "Ramesh Iyer",
+        "fuel_type": "Electric",
+        "transmission": "Semi-Automatic",
+        "pricePerDay": 900,
+        "location": "Hydrabad",
+        "description": "The Ather 450X is an electric scooter manufactured by Ather Energy in India. It was launched in Bengaluru in September 2018.[2][3] It is powered by a 5.4 kW (7.2 BHP) Brushless DC electric motor, and a 3.7 kWh lithium-ion battery pack. The scooter can accelerate 0–40 km/h (0–25 mph) in 3.3 seconds. It has a top speed of 90 km/h (56 mph) with a Certified Range of 146 km (91 mi). ",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
+    {
+        "_id": "67ff5bc069c03d4e45f30b45",
+        "owner": "67fe3467ed8a8fe17d0ba6e2",
+        "brand": "Hero",
+        "model": "Super Splendor",
+        "image": car_image12,
+        "year": 2021,
+        "category": "Bike",
+        "user_name": "Ganesh Talpade",
+        "fuel_type": "Petrol",
+        "transmission": "Manual",
+        "pricePerDay": 800,
+        "location": "Mumbai",
+        "description": " The Hero Super Splendor is a 125cc commuter motorcycle designed for everyday use, known for its reliability, fuel efficiency, and low maintenance costs. It is powered by a 124.7 cc, air-cooled, single-cylinder engine that produces 10.7 PS of power and 10.6 Nm of torque, paired with a 5-speed manual transmission. The bike offers a claimed mileage of approximately 60 kmpl, making it an economical choice for daily commuting.",
+        "isAvaliable": true,
+        "createdAt": "2025-04-16T07:26:56.215Z",
+    },
 ];
 
 export const dummyMyBookingsData = [
